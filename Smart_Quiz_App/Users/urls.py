40a3f7c596b38919fbb5b7bcfabda7a0b5561256
abhishekkushwaha4u <thecrazycoderabhi@gmail.com>
2019-05-19 20:16:45 +0530

    Updated, aaded user login, signup and authentication

--- Smart_Quiz_App/Users/urls.py
+++ Smart_Quiz_App/Users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView,LogoutView
+
+app_name = "Users"
 urlpatterns = [
-    path('',views.home ),
-    path('login/',LoginView.as_view(template_name="Users/login.html")),
-    path('logout/',LogoutView.as_view(template_name="Users/logout.html")),
+    path('',views.home,name="homepage" ),
+    path('login/',LoginView.as_view(template_name="Users/login.html"),name="login"),
+    path('logout/', LogoutView.as_view(template_name="Users/logout.html"), name="logout"),
+    path('signup/',views.signup,name="signup"),
 ]
--- Smart_Quiz_App/Users/views.py
+++ Smart_Quiz_App/Users/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 def home(request):
     return render(request,"Users/home.html")
+
+def signup(request):
+    if request.method=="POST":
+        newUser = UserCreationForm(request.POST)
+        if newUser.is_valid():
+            user = newUser.save()
+            user.is_staff = True
+            login(request,user)
+            return redirect("Users:homepage")
+        else:
+            for message in newUser.error_messages:
+                print(newUser.error_messages[message])
+    newUser = UserCreationForm
+    return render(request,"Users/signup.html",{"newUser":newUser})
+
