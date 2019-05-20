d3a48d48847a211dd494301ee5e3eac66629b0e2
abhishekkushwaha4u <thecrazycoderabhi@gmail.com>
2019-05-20 15:04:48 +0530

    minor changes, added basic profile model

--- Smart_Quiz_App/Users/urls.py
+++ Smart_Quiz_App/Users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView,LogoutView
+# from django.contrib.auth.views import LoginView,LogoutView
 
 app_name = "Users"
 urlpatterns = [
     path('',views.home,name="homepage" ),
-    path('login/',LoginView.as_view(template_name="Users/login.html"),name="login"),
-    path('logout/', LogoutView.as_view(template_name="Users/logout.html"), name="logout"),
+    path('login/',views.login_request,name="login"),
+    path('logout/',views.logout_request, name="logout"),
     path('signup/',views.signup,name="signup"),
 ]
--- Smart_Quiz_App/Users/views.py
+++ Smart_Quiz_App/Users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login,logout,authenticate
 from django.contrib import messages
 def home(request):
@@ -10,12 +10,39 @@ def signup(request):
         newUser = UserCreationForm(request.POST)
         if newUser.is_valid():
             user = newUser.save()
-            user.is_staff = True
+            username = newUser.cleaned_data.get('username')
+            messages.success(request,f"Account created for : {username}")
             login(request,user)
+            messages.success(request, f"You have logged in as : {username}")
             return redirect("Users:homepage")
         else:
             for message in newUser.error_messages:
-                print(newUser.error_messages[message])
+                messages.error(request,f"{message}:{newUser.error_messages[message]}")
     newUser = UserCreationForm
     return render(request,"Users/signup.html",{"newUser":newUser})
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request,template_name="Users/login.html",context={"form": form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request,"Logged out successfully !")
+    return render(request,"Users/logout.html")
+
 
